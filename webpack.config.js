d74db4a38d7472923249d019af33113619e3bc17
Yannick Fricke <yannickfricke@googlemail.com>
2018-11-21 22:52:27 +0100

    build(webpack.config.js): Updated webpack config for electron
    Added the html-webpack-plugin and modified the targets for the use with electron

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
 const commonConfig = {
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -9,17 +11,37 @@ const commonConfig = {
             {
                 test: /\.tsx?$/,
                 loader: 'awesome-typescript-loader'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
             }
         ]
     },
     resolve: {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    },
+    node: {
+        __dirname: false
     }
 };
 
-module.exports = Object.assign({
-    entry: {
-        main: './src/main.ts'
-    },
-    mode: 'production'
-}, commonConfig);
+module.exports = [
+    Object.assign({
+        target: 'electron-main',
+        entry: {
+            main: './src/main.ts'
+        },
+        mode: 'none'
+    }, commonConfig),
+    Object.assign({
+        target: 'electron-renderer',
+        entry: {
+            gui: './src/gui.tsx'
+        },
+        plugins: [new htmlWebpackPlugin({
+            template: 'src/index.html'
+        })],
+        mode: 'none'
+    }, commonConfig)
+];
