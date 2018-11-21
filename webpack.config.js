59ebfa022fa1cbf8eb3d909ceb696703c3652d92
Yannick Fricke <yannickfricke@googlemail.com>
2018-11-21 22:51:10 +0100

    build(webpack.config.js): Use awesome-typescript-loader instead of ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const commonConfig = {
         rules: [
             {
                 test: /\.tsx?$/,
-                loader: 'ts-loader'
+                loader: 'awesome-typescript-loader'
             }
         ]
     },
