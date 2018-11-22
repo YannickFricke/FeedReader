0240e69326434c622234918eeb039f1ca62f8c38
Yannick Fricke <yfr@mts.de>
2018-11-22 15:50:10 +0100

    build(webpack): Use different configs for webpack
    Use different configuration files for the development and production packing of webpack. Also
    changed the CSP meta tag to allow 'unsafe-eval'

---
+++ webpack.dev.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+const commonConfig = {
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    },
+    node: {
+        __dirname: false
+    }
+};
+
+module.exports = [
+    Object.assign({
+        target: 'electron-main',
+        entry: {
+            main: './src/main.ts'
+        },
+        mode: 'development'
+    }, commonConfig),
+    Object.assign({
+        target: 'electron-renderer',
+        entry: {
+            gui: './src/gui.tsx'
+        },
+        plugins: [new htmlWebpackPlugin({
+            template: 'src/index.html'
+        })],
+        mode: 'development'
+    }, commonConfig)
+];
