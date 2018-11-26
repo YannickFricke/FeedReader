d42c6001449931300687ab5bfa66e322a6b1b3a4
Yannick Fricke <yannickfricke@googlemail.com>
2018-11-27 00:05:34 +0100

    ci(Travis): Some changes for the config
    Moved the script out to an extra file. The Mac OS build script also needs to transpile the
    TypeScript files before packaging

---
+++ ci/travis.sh
@@ -0,0 +1,11 @@
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    docker run --rm \
+    --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
+    -v ${PWD}:/project \
+    -v ~/.cache/electron:/root/.cache/electron \
+    -v ~/.cache/electron-builder:/root/.cache/electron-builder \
+    electronuserland/builder:wine \
+    /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn build:prod && yarn package"
+else
+    yarn run build:prod && yarn run electron-builder
+fi
