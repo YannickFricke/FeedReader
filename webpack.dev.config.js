6cfff4f048fae3db10b8c7157592500245171ba7
Yannick Fricke <yfr@mts.de>
2018-11-26 12:02:36 +0100

    build(webpack): Added SASS support
    Added missing rules for the scss, css and font files. Also modified the CSP meta tag to allow the
    css from the webpack-dev-server. The prod config now uses the mini-css-extract-plugin for CSS.

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -15,11 +15,42 @@ const commonConfig = {
             {
                 test: /\.html$/,
                 loader: 'html-loader'
+            },
+            {
+                test: /\.s[a|c]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+                use: 'file-loader?name=[name].[ext]?[hash]'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.otf(\?.*)?$/,
+                use: 'file-loader?name=/fonts/[name].  [ext]&mimetype=application/font-otf'
             }
         ]
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.scss']
     },
     node: {
         __dirname: false
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const commonConfig = {
     output: {
@@ -15,11 +16,42 @@ const commonConfig = {
             {
                 test: /\.html$/,
                 loader: 'html-loader'
+            },
+            {
+                test: /\.s[a|c]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.jpe?g$|\.gif$|\.ico$|\.png$|\.svg$/,
+                use: 'file-loader?name=[name].[ext]?[hash]'
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+            },
+            {
+                test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'file-loader'
+            },
+            {
+                test: /\.otf(\?.*)?$/,
+                use: 'file-loader?name=/fonts/[name].  [ext]&mimetype=application/font-otf'
             }
         ]
     },
     resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.scss']
     },
     node: {
         __dirname: false
@@ -39,9 +71,15 @@ module.exports = [
         entry: {
             gui: './src/gui.tsx'
         },
-        plugins: [new htmlWebpackPlugin({
-            template: 'src/index.html'
-        })],
+        plugins: [
+            new htmlWebpackPlugin({
+                template: 'src/index.html'
+            }),
+            new miniCssExtractPlugin({
+                filename: '[name].css',
+                chunkFilename: '[id].css'
+            })
+        ],
         mode: 'production'
     }, commonConfig)
 ];
