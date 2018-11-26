a7e29e8e5de2c684b6deda5c2e62d5ee714b0077
Yannick Fricke <yannickfricke@googlemail.com>
2018-11-26 20:54:59 +0100

    style(App): Enforced code style for the app component

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 
-export class App extends React.Component<{}, {}>
-{
-    render()
-    {
-        return <div>Hello from FeedReader</div>;
-    }
+export class App extends React.Component<{}, {}> {
+	public render() {
+		return <div>Hello from FeedReader</div>;
+	}
 }
