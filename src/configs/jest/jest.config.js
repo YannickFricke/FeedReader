78092230e69cfd615865ffe8af76f5b1a9475056
Yannick Fricke <yfr@mts.de>
2018-11-27 16:40:06 +0100

    chore(Jest): Updated the Jest configuration
    Jest needs a setup file for setting up the enzyme-adapter properly. Also added the code coverage
    directory to the excludes files in the tsconfig.json

--- src/configs/jest/jest.config.js
+++ src/configs/jest/jest.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/ts/**/*.{ts,tsx}',
-    '!**/node_modules/**',
-    '!**/vendor/**'
-  ],
-  coverageDirectory: './code-cov/'
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	collectCoverage: true,
+	collectCoverageFrom: [
+		'src/ts/**/*.{ts,tsx}',
+		'!**/node_modules/**',
+		'!**/vendor/**'
+	],
+	coverageDirectory: './code-cov/',
+	setupFiles: [
+		'./src/configs/jest/test-setup.ts',
+		'./src/configs/jest/test-shim.js'
+	],
+	snapshotSerializers: ["enzyme-to-json"]
 };
