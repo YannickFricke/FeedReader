25f4580dd73cc481bc7d3d33e1ed5a6fa15f7460
Yannick Fricke <yannickfricke@googlemail.com>
2018-11-28 06:52:27 +0100

    feat(Strings): Added random string generator + test

---
+++ src/ts/definitions/__tests__/Strings.test.ts
@@ -0,0 +1,16 @@
+import {generateRandomString} from '../Strings';
+
+it('should generate a random string', () => {
+    const generatedString = generateRandomString();
+
+    expect(generatedString).not.toBeUndefined();
+    expect(generatedString).toHaveLength(32);
+})
+
+it('should generate a random string with a specific length', () => {
+    const length = 16;
+    const generatedString = generateRandomString(length);
+
+    expect(generatedString).not.toBeUndefined();
+    expect(generatedString).toHaveLength(length);
+})
