68e8c82f368c6dca0748641e67c4594dc8a6c17d
Yannick Fricke <yfr@mts.de>
2019-01-25 16:25:51 +0100

    build(webpack): Use the tslint-loader in the development config
    Also made some style fixes

--- src/configs/webpack/webpack.dev.config.js
+++ src/configs/webpack/webpack.dev.config.js
@@ -1,30 +1,50 @@
-const path = require('path');
+const path              = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
-const commonConfig = require('./webpack.common.config');
+const commonConfig      = require('./webpack.common.config');
+
+const electronMain = Object.assign({
+    target: 'electron-main',
+    entry : {
+        main: path.resolve(__dirname, '..', '..', 'main.ts')
+    },
+    mode: 'development'
+}, commonConfig);
+
+const electronRenderer = Object.assign({
+    target: 'electron-renderer',
+    entry : {
+        gui: path.resolve(__dirname, '..', '..', 'gui.tsx'),
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, '..', '..', 'index.html')
+        })
+    ],
+    mode     : 'development',
+    devServer: {
+        contentBase: path.join(__dirname, '..', '..', '..', 'dist'),
+        hot        : true,
+        inline     : true,
+        port       : 8080,
+        stats      : 'normal'
+    }
+}, commonConfig);
+
+const tsLintLoader = {
+    loader : 'tslint-loader',
+    options: {
+        fix       : true,
+        configFile: 'tslint.json',
+        emitErrors: true,
+        typeCheck : true
+    }
+};
+
+// Push the tslint loader
+electronMain.module.rules[0].use.push(tsLintLoader);
+electronRenderer.module.rules[0].use.push(tsLintLoader);
 
 module.exports = [
-    Object.assign({
-        target: 'electron-main',
-        entry: {
-            main: path.resolve(__dirname, '..', '..', 'main.ts')
-        },
-        mode: 'development'
-    }, commonConfig),
-    Object.assign({
-        target: 'electron-renderer',
-        entry: {
-            gui: path.resolve(__dirname, '..', '..', 'gui.tsx'),
-        },
-        plugins: [new htmlWebpackPlugin({
-            template: path.resolve(__dirname, '..', '..', 'index.html')
-        })],
-        mode: 'development',
-        devServer: {
-            contentBase: path.join(__dirname, '..', '..', '..', 'dist'),
-            hot: true,
-            inline: true,
-            port: 8080,
-            stats: 'normal'
-        }
-    }, commonConfig)
+    electronMain,
+    electronRenderer
 ];
