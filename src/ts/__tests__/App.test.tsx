05b565124931a371b3e626f0d1cc949986c6d70b
Yannick Fricke <yfr@mts.de>
2019-01-29 12:24:30 +0100

    refactor(App): Removed the test code
    Also updated the test for the App component

--- src/ts/__tests__/App.test.tsx
+++ src/ts/__tests__/App.test.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 import {App} from '../App';
 
 it('renders the app', () => {
-	const result = shallow(<App />);
-	expect(result).toBeTruthy();
+    const result = shallow(<App />);
+
+    expect(result).toBeTruthy();
+    expect(result.text()).toContain('Got the following configuration path: mocked_directory');
 });
