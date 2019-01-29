835c433a85543174f533939827b65a1ae9639ceb
Yannick Fricke <yfr@mts.de>
2019-01-29 13:23:50 +0100

    docs(Strings): Added docs for the "generateRandomString" function

--- src/ts/helper/Strings.ts
+++ src/ts/helper/Strings.ts
@@ -1,3 +1,7 @@
+/**
+ * Generates a random string with the given length
+ * @param length Length of the string to generate
+ */
 export function generateRandomString(length: number = 32): string {
     const result: string[] = [];
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
