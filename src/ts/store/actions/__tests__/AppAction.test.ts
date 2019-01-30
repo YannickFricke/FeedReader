9bb235e360f56014b2b6b5a9f73c62d98940bc52
Yannick Fricke <yfr@mts.de>
2019-01-30 14:30:11 +0100

    test(AppAction): Added the test for the "TOGGLE_DARKMODE" action
    Also applied some style fixes from tslint.

--- src/ts/store/actions/__tests__/AppAction.test.ts
+++ src/ts/store/actions/__tests__/AppAction.test.ts
@@ -1,14 +1,23 @@
-import {FETCH_FEEDS} from '../AppAction';
+import {FETCH_FEEDS, TOGGLE_DARKMODE} from '../AppAction';
 
 it('Checks the FetchFeeds action', () => {
-    const result = FETCH_FEEDS();
+    const result = FETCH_FEEDS;
 
     // The result should not be undefined
     expect(result).not.toBeUndefined();
-    
+
     // Check if the results matches
     expect(result).toMatchObject({
-		status: 'STATUS_FETCH_FEEDS',
-		type: 'FETCHING_FEEDS',
-	});
+        status: 'STATUS_FETCH_FEEDS',
+        type: 'FETCHING_FEEDS',
+    });
+});
+
+it('Checks the TOGGLE_DARKMODE action', () => {
+    const result = TOGGLE_DARKMODE;
+
+    expect(result).not.toBeUndefined();
+    expect(result).toMatchObject({
+        type: 'TOGGLE_DARKMODE',
+    });
 });
