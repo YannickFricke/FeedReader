d5bb66181f3f21e0e2f8e8fc215d0fb2dc81f2de
Yannick Fricke <yfr@mts.de>
2019-01-30 14:25:45 +0100

    style(Store.test): Applied the coding style from tslint

--- src/ts/store/__tests__/Store.test.ts
+++ src/ts/store/__tests__/Store.test.ts
@@ -1,28 +1,28 @@
-import {createDefaultStore} from '../Store';
 import {initialState} from '../reducers/AppReducer';
+import {createDefaultStore} from '../Store';
 
 // Declare the "global" variable as type "any" for testing purposes
 declare var global: any;
 
 describe('Store tests', () => {
-    it('returns an store with devtools', () => {    
+    it('returns an store with devtools', () => {
         global.window = {
-            devToolsExtension: jest.fn()
+            devToolsExtension: jest.fn(),
         };
-    
+
         const result = createDefaultStore();
-    
+
         expect(result).not.toBeUndefined();
         expect(result.getState()).toMatchObject(initialState);
     });
 
     it('returns an store without devtools', () => {
         global.window = {
-            devToolsExtension: undefined
+            devToolsExtension: undefined,
         };
-    
+
         const result = createDefaultStore();
-    
+
         expect(result).not.toBeUndefined();
         expect(result.getState()).toMatchObject(initialState);
     });
