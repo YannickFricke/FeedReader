3fc4902fd57f2aca8fb62e1a91d700fa046ee94e
Yannick Fricke <yannickfricke@googlemail.com>
2019-01-31 23:56:53 +0100

    feat(Menu): Added the basic menu component

---
+++ src/ts/components/Menu.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class Menu extends React.Component {
+    public render() {
+        return <ul></ul>;
+    }
+}
--- src/ts/components/Sidebar.tsx
+++ src/ts/components/Sidebar.tsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 
-export class Sidebar extends Component<{}, {}> {
+export class Sidebar extends React.Component {
     public render() {
         return <aside>
             <div id="addContainer">
                 <div id={'logo'} />
+                <div id="heading">FeedReader</div>
             </div>
         </aside>;
     }
