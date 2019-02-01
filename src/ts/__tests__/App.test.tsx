c39275e1d297289db50799da826e0aab2498a35d
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-01 19:41:07 +0100

    test(App): Added test for the "toggleDarkMode" function

--- src/ts/__tests__/App.test.tsx
+++ src/ts/__tests__/App.test.tsx
@@ -8,4 +8,16 @@ describe('App component', () => {
 
         expect(result).toBeTruthy();
     });
+
+    it('should invoke the TOGGLE_DARKMODE function when clicking on the content', () => {
+        const toggleDarkMode = jest.fn();
+        const result = mount(<App darkmode={false} toggleDarkmode={toggleDarkMode} />);
+        // tslint:disable-next-line:no-console
+        console.log(result.state());
+
+        result.find('#content').simulate('click');
+
+        expect(toggleDarkMode).toBeCalled();
+        expect(toggleDarkMode).toBeCalledTimes(1);
+});
 });
