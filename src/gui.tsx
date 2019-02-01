a8131c35398d84690c1a6a5764563c9432847218
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-01 19:13:37 +0100

    refactor(GUI): Added the global styles from the SCSS file

--- src/gui.tsx
+++ src/gui.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 
 // Provides the store (state) to all subcomponents
 import { Provider } from 'react-redux';
@@ -10,13 +11,24 @@ import { createDefaultStore } from './ts/store/Store';
 // Application components
 import App from './ts/App';
 
-// Load the styles
-import './scss/main.scss';
 import { createBrowserHistory } from 'history';
+
+const GLOBALSTYLE = createGlobalStyle`
+    html,
+    body,
+    #root {
+        font-family: Arial, Helvetica, sans-serif;
+        padding    : 0;
+        margin     : 0;
+        height     : 100vh;
+        width      : 100vw;
+    }
+`;
 
 ReactDom.render(
     <Provider store={createDefaultStore(createBrowserHistory())}>
-        <App/>
+        <GLOBALSTYLE />
+        <App />
     </Provider>,
     document.getElementById('root'),
 );
