0f04b6ed14c066080b89e6333d4bf2ea97ada674
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-02 00:08:06 +0100

    feat(Theme): Added the basic classes for theming
    Added the ITheme and the IThemeConfiguration interfaces

---
+++ src/ts/theme/ITheme.ts
@@ -0,0 +1,8 @@
+export interface ITheme {
+    /**
+     * When set to true the darkmode is activated
+     * @author Yannick Fricke <yannickfricke@googlemail.com>
+     * @license MIT
+     */
+    darkmode: boolean;
+}
