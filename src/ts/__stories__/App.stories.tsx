ef1bd66b4b48d48e1a1c3edfc3a0758338d3995c
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-01 18:20:18 +0100

    refactor(App): Removed the story for the App component

--- src/ts/__stories__/App.stories.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import {storiesOf} from '@storybook/react';
-import {App} from '../App';
-
-storiesOf('App', module)
-	.add('Without properties', () => {
-		return <App />;
-	});
