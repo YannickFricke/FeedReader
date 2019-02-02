1633c0f51b4e15980509a8637131e419f1d06d09
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-02 15:11:45 +0100

    ci(Danger): Check reviewers and style changes
    Added a function for checking the count of reviewers and all messages now contain an emoji

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,22 +1,42 @@
-import { danger, fail, message } from 'danger';
+import { danger, fail, GitHubUser, message, warn } from 'danger';
+
+const Symbols = {
+    changed     : ':pencil:',
+    construction: ':construction:',
+    o           : ':o:',
+    ok          : ':heavy_check_mark:',
+    warning     : ':warning:',
+    x           : ':heavy_multiplication_x:',
+};
 
 writeModifiedFiles();
 checkAssignee();
 checkWIPStatus();
+checkReviewers();
 
 function writeModifiedFiles() {
     const modifiedFiles = danger.git.modified_files.join('\n- ');
-    message('Changed Files in this PR: \n- ' + modifiedFiles);
+    message(`${Symbols.changed} Changed Files in this PR: \n- ` + modifiedFiles);
 }
 
 function checkAssignee() {
     if (!danger.github.pr.assignee) {
-        fail('This pull request needs an assignee, and optionally include any reviewers.');
+        fail(`${Symbols.x} This pull request needs an assignee!`);
     }
 }
 
 function checkWIPStatus() {
     danger.github.pr.title.includes('WIP') ?
-        fail('Pull request is currently in work!') :
-        message(':heavy_check_mark: Pull request is ready to merge!');
+        fail(`${Symbols.construction} Pull request is currently in work!`) :
+        message(`${Symbols.ok} Pull request is ready to merge!`);
+}
+
+function checkReviewers() {
+    const reviewers = danger.github.requested_reviewers.map((gitHubUser: GitHubUser) => {
+        return `- ${gitHubUser.login}`;
+    });
+
+    danger.github.requested_reviewers.length === 0 ?
+    warn(`${Symbols.warning} There are no reviewers assigned to this pull request!`) :
+    message(`${Symbols.ok} Assigned reviewers: \n- @${reviewers.join('- @')}`);
 }
