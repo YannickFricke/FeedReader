282939bdc6955df85558ac18a07838c309f02924
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-02 06:48:59 +0100

    ci(Danger): Added more functions for pr
    Now we check for an assignee and for the "WIP" keyword in the title

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,8 +1,22 @@
-import { danger, message } from 'danger';
+import { danger, fail, message } from 'danger';
 
 writeModifiedFiles();
+checkAssignee();
+checkWIPStatus();
 
 function writeModifiedFiles() {
     const modifiedFiles = danger.git.modified_files.join('\n- ');
     message('Changed Files in this PR: \n- ' + modifiedFiles);
+}
+
+function checkAssignee() {
+    if (!danger.github.pr.assignee) {
+        fail('This pull request needs an assignee, and optionally include any reviewers.');
+    }
+}
+
+function checkWIPStatus() {
+    danger.github.pr.title.includes('WIP') ?
+        fail('Pull request is currently in work!') :
+        message(':heavy_check_mark: Pull request is ready to merge!');
 }
