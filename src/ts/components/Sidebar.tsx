c05b3bc818b1b3e9d3c20477982b1cbd9d9c16b8
Yannick Fricke <yfr@mts.de>
2019-02-04 17:53:06 +0100

    refactor(Sidebar): Refatored to a styled component

--- src/ts/components/Sidebar.tsx
+++ src/ts/components/Sidebar.tsx
@@ -1,12 +1,38 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import Logo from '../../icons/png/32x32.png';
+import { IThemeConfiguration } from '../theme/IThemeConfiguration';
 
-export class Sidebar extends React.Component {
+const Sidebar = styled.aside`
+    & {
+        border-right: 1px solid ${(props: IThemeConfiguration) => {
+            return props.theme.darkmode ? '#f0f0f0' : '#212121';
+        }};
+    }
+
+    #addContainer {
+        border-bottom: 1px solid ${(props: IThemeConfiguration) => {
+            return props.theme.darkmode ? '#f0f0f0' : '#212121';
+        }};
+        width: auto;
+        padding: 15px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    #heading {
+        font-size: 25px;
+    }
+`;
+
+export default class extends React.Component {
     public render() {
-        return <aside>
+        return <Sidebar>
             <div id="addContainer">
-                <div id={'logo'} />
+                <img src={Logo} alt={'logo'} width={'32px'} height={'32px'} />
                 <div id="heading">FeedReader</div>
             </div>
-        </aside>;
+        </Sidebar>;
     }
 }
