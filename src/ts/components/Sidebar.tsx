dfc17108800d38d83b6f092ea4b9528bbe7145e7
Yannick Fricke <yfr@mts.de>
2019-02-05 16:15:23 +0100

    refactor(Sidebar): Refactored structure to styled-components
    Refactored the children of the Sidebar component to be styled components

--- src/ts/components/Sidebar.tsx
+++ src/ts/components/Sidebar.tsx
@@ -1,36 +1,40 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import Logo from '../../icons/png/32x32.png';
+import LogoImage from '../../icons/png/32x32.png';
 import { IThemeConfiguration } from '../theme/IThemeConfiguration';
 
 const SidebarWrapper = styled.aside`
     border-right: 1px solid ${(props: IThemeConfiguration) => {
         return props.theme.darkmode ? '#f0f0f0' : '#212121';
     }};
+`;
 
-    #addContainer {
-        border-bottom: 1px solid ${(props: IThemeConfiguration) => {
-            return props.theme.darkmode ? '#f0f0f0' : '#212121';
-        }};
-        width: auto;
-        padding: 15px;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-    }
+const SidebarContainer = styled.div`
+    border-bottom: 1px solid ${(props: IThemeConfiguration) => {
+        return props.theme.darkmode ? '#f0f0f0' : '#212121';
+    }};
+    width: auto;
+    padding: 15px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
 
-    #heading {
-        font-size: 25px;
-    }
+const Logo = styled.img`
+    margin-right: 10px;
+`;
+
+const Heading = styled.div`
+    font-size: 25px;
 `;
 
 export class Sidebar extends React.Component {
     public render() {
         return <SidebarWrapper>
-            <div id="addContainer">
-                <img src={Logo} alt={'logo'} width={'32px'} height={'32px'} />
-                <div id="heading">FeedReader</div>
-            </div>
+            <SidebarContainer>
+                <Logo id={'logo'} src={LogoImage} alt={'logo'} width={'32px'} height={'32px'} />
+                <Heading>FeedReader</Heading>
+            </SidebarContainer>
         </SidebarWrapper>;
     }
 }
