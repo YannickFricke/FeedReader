3823b2f23e6b2c45ffc6bd43a7d7f0a27d583d58
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-07 22:52:39 +0100

    docs(StoreType): Added docs for the states

--- src/ts/store/StoreType.ts
+++ src/ts/store/StoreType.ts
@@ -2,11 +2,25 @@ import { RouterState } from 'connected-react-router';
 import { IFeed } from '../definitions/IFeed';
 
 export interface IApplicationState {
+    /**
+     * Contains the application state
+     */
     app: IAppStoreState;
+
+    /**
+     * Contains the router state
+     */
     router: RouterState;
 }
 
 export interface IAppStoreState {
+    /**
+     * Contains all loaded feeds
+     */
     feeds: IFeed[];
+
+    /**
+     * When set to true the app will be displayed with dark components and dark backgrounds
+     */
     darkmode: boolean;
 }
