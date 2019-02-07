f6239c59e6bb253018e2504029603d89bc7dc7f5
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-07 23:15:20 +0100

    docs(AppAction): Added docs for all actions

--- src/ts/store/actions/AppAction.ts
+++ src/ts/store/actions/AppAction.ts
@@ -1,14 +1,23 @@
 import { Action } from 'redux';
 
+/**
+ * Contains all properties for the actions
+ */
 export interface IAppAction extends Action {
     status?: string;
 }
 
+/**
+ * Updates all known feeds
+ */
 export const FETCH_FEEDS: IAppAction = {
     status: 'STATUS_FETCH_FEEDS',
     type: 'FETCHING_FEEDS',
 };
 
+/**
+ * Toggles the darkmode of the application
+ */
 export const TOGGLE_DARKMODE: Action = {
     type: 'TOGGLE_DARKMODE',
 };
