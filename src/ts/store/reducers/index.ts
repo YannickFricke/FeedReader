d2e47b31e609374a73e831355347904bacf86a88
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-07 23:10:18 +0100

    docs(Reducers): Added docs for the default export
    Added docs for the exported reducers

--- src/ts/store/reducers/index.ts
+++ src/ts/store/reducers/index.ts
@@ -3,6 +3,9 @@ import { History } from 'history';
 import { combineReducers } from 'redux';
 import { AppReducer } from './AppReducer';
 
+/**
+ * Returns the combined reducers
+ */
 export default (history: History) => combineReducers({
     router: connectRouter(history),
     app: AppReducer,
