3c6fbf76b8a23265f267180bb435d613993495b7
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-08 00:20:18 +0100

    docs(ITheme): Updated documentation for the ITheme interface

--- src/ts/theme/ITheme.ts
+++ src/ts/theme/ITheme.ts
@@ -1,8 +1,12 @@
+/**
+ * Contains all properties for the application theme
+ * @author Yannick Fricke <yannickfricke@googlemail.com>
+ * @license MIT
+ */
 export interface ITheme {
     /**
      * When set to true the darkmode is activated
      * @author Yannick Fricke <yannickfricke@googlemail.com>
-     * @license MIT
      */
     darkmode: boolean;
 }
