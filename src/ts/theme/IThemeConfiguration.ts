5d9b3701068d0f87682ac465461f956b9bb82066
Yannick Fricke <yannickfricke@googlemail.com>
2019-02-08 00:20:53 +0100

    docs(IThemeConfiguration): Updated docs for the IThemeConfiguration interface

--- src/ts/theme/IThemeConfiguration.ts
+++ src/ts/theme/IThemeConfiguration.ts
@@ -1,11 +1,16 @@
 import { ITheme } from './ITheme';
 
+/**
+ * Contains the theme configuration for styled-components
+ *
+ * @author Yannick Fricke <yannickfricke@googlemail.com>
+ * @license MIT
+ */
 export interface IThemeConfiguration {
     /**
      * Contains the theme for the application
      *
      * @author Yannick Fricke <yannickfricke@googlemail.com>
-     * @license MIT
      */
     theme: ITheme;
 }
